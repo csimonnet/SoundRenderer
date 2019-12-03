588d859a7facebda3ce93ee27e2be99e6e8d3499
csimonnet <charlotte.simonnet@gmail.com>
2019-12-03 16:15:31 +0100

    feat: Adding square and sine type | :sparkles:

--- concert-master.js
+++ concert-master.js
@@ -1,17 +1,21 @@
 import { OscillatorVisualizator } from "./visualizator.js";
 import { SoundAnalyser } from "./analyser.js";
+import { Synthesizer } from "./synth.js";
 
 const SOURCE_TYPE_SYNTH = "synth";
 
 class ConcertMaster {
-    constructor(source, visualizatorType, canvasElement) {
+    constructor(source, visualizatorType, canvasElement, synthElement) {
         this.audioContext = new AudioContext();
         
         if (source === SOURCE_TYPE_SYNTH) {
-            const osc1 = this.audioContext.createOscillator();
+            this.synthesizer = new Synthesizer(this.audioContext);
+            this.source = this.synthesizer.getSource();
+            synthElement.addEventListener('change', (event) => {
+                this.synthesizer.onChange(event)
+            });
         } else if (source instanceof HTMLAudioElement) {
             this.source = this.audioContext.createMediaElementSource(source);
-            source.onplay = () => this.playFrame();
         }
         
         switch (visualizatorType) {
@@ -22,11 +26,16 @@ class ConcertMaster {
         this.analyser = new SoundAnalyser(this.audioContext);
         this.source.connect(this.analyser.getWebAnalyser());
         this.source.connect(this.audioContext.destination);
+        this.playFrame();
 
     }
 
     playFrame() {
+       
        requestAnimationFrame(() => this.playFrame());
+       if(this.analyser === undefined) {
+           return false;
+       }
        const soundData = this.analyser.getData();
        this.visualizator.justDrawIt(soundData);
     }
--- synth.js
+++ synth.js
@@ -1,8 +1,25 @@
-class Synth {
-    constructor(visualizator) {
-        this.audioContext = new AudioContext();
+const OSC_1_WAVE_TYPE = "osc-1-wavetype";
+
+class Synthesizer {
+    constructor(audioContext) {
+        this.audioContext = audioContext;
+        this.oscillator1 = this.audioContext.createOscillator();
+        this.oscillator1.type = 'sine';
+        this.oscillator1.start();
+    }
+
+    onChange(event) {
+        let changedElement = event.target;
+        if (changedElement.id === OSC_1_WAVE_TYPE) {
+            this.oscillator1.type = changedElement.value;
+        }
+    }
+
+    getSource()
+    {
+        return this.oscillator1;
     }
 
 }
 
-export { Synth };
+export { Synthesizer };
